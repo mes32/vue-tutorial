626968eb03f184a03f4abce741477efe216e5370
mes32 <stockman.mike@gmail.com>
2019-07-22 09:34:50 -0500

    Add component EmployeeTable

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <EmployeeTable />
   </div>
 </template>
 
 <script>
+import EmployeeTable from './components/EmployeeTable'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
+    EmployeeTable,
     HelloWorld
   }
 }
---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="employee-table">
+    <table>
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Ada Lovelace</td>
+          <td>ada@vue-tutorial.com</td>
+        </tr>
+        <tr>
+          <td>Alan Turing</td>
+          <td>alan@vue-tutorial.com</td>
+        </tr>
+        <tr>
+          <td>John von Neumann</td>
+          <td>john@vue-tutorial.com</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'employee-table',
+}
+</script>
+
+<style scoped></style>
